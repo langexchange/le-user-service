5e2dda3d62ca7b153f242a5dfb9836ec69b32fb0
Nguyễn Văn Thương <thuong.nguyen@deliveree.com>
2023-02-14 23:19:54 +0700

    gen new models

---
+++ src/le-user-service/LE.UserService.Infrastructure/Infrastructure/Entities/Targetlang.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace LE.UserService.Infrastructure.Infrastructure.Entities
+{
+    public partial class Targetlang
+    {
+        public Guid Userid { get; set; }
+        public Guid Langid { get; set; }
+
+        public virtual Language Lang { get; set; }
+        public virtual User User { get; set; }
+    }
+}
