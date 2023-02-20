c813dd01ee897acf04741c9556d7820a8be98dd6
Nguyễn Văn Thương <80138135+ThuongNguyenVan158@users.noreply.github.com>
2023-02-20 17:39:07 +0700

    Features/posting (#4)
    * init post service
    
    * Update PostService.cs

---
+++ src/le-user-service/LE.UserService/Dtos/PostDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LE.UserService.Dtos
+{
+    public class PostDto
+    {
+        public PostDto()
+        {
+            ImagePost = new List<FileOfPost>();
+            AudioPost = new List<FileOfPost>();
+            VideoPost = new List<FileOfPost>();
+        }
+        public Guid UserId { get; set; }
+        public Guid LangId { get; set; }
+        public string Text { get; set; }
+        public List<FileOfPost> ImagePost { get; set; }
+        public List<FileOfPost> AudioPost { get; set; }
+        public List<FileOfPost> VideoPost { get; set; }
+    }
+
+    public class FileOfPost
+    {
+        public string Type { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ src/le-user-service/LE.UserService/Services/Implements/PostService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using LE.UserService.Dtos;
+using LE.UserService.Infrastructure.Infrastructure;
+using LE.UserService.Infrastructure.Infrastructure.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LE.UserService.Services.Implements
+{
+    public class PostService /*: IPostService*/
+    {
+        private LanggeneralDbContext _context;
+        private readonly IMapper _mapper;
+
+        public PostService(LanggeneralDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        //public Task CreatePost(PostDto postDto, CancellationToken cancellationToken = default)
+        //{
+        //    var post = new Post
+        //    {
+        //        Postid = Guid.NewGuid(),
+        //        Langid = postDto.LangId,
+        //        Userid = postDto.UserId,
+        //        //IsAudio = postDto.AudioPost.Count > 0 ? 1 : 0;
+        //    };
+        //}
+    }
+}
