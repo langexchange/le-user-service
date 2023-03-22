bb9ab0dfa56477552663ea8819e4f6e25130e811
Nguyễn Văn Thương <nvthuong1508@gmail.com>
2023-03-22 12:01:57 +0700

    Update Env.cs

--- src/le-user-service/LE.UserService/Env.cs
+++ src/le-user-service/LE.UserService/Env.cs
@@ -5,7 +5,7 @@ namespace LE.UserService
     public static class Env
     {
         public readonly static string DB_CONNECTION_STRING = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-        public readonly static string SECRET_KEY = Environment.GetEnvironmentVariable("SECRET_KEY");
+        public readonly static string SECRET_KEY = Environment.GetEnvironmentVariable("SECRET_KEY") ?? string.Empty;
         public const string AZNormalChars = "abcdefghijklmntuvwvz";
         public const string NumberChars = "1234567890";
         public const string SplitChar = "#";
